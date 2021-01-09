09d502c109a6a2c60e6ddd6d40c36f20ce7c665a
jakobRhodes <46934743+jakobRhodes@users.noreply.github.com>
2021-01-09 14:46:03 -0700

    t

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var ol = document.getElementById("ol");
+var array = ["week1/index.html", "week2/index.html"];
+var li = document.createElement("li");
+li.appendChild(document.createTextNode('test'));
+ol.appendChild(li);
