45b5736bbd23e8d64222a1490596e4e1bce1db08
jakobRhodes <46934743+jakobRhodes@users.noreply.github.com>
2021-01-30 17:59:53 -0700

    Updated

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
-var ol = document.getElementById("ol");
-var array = ["week1/index.html", "week2/index.html"];
-var li = document.createElement("li");
-li.appendChild(document.createTextNode('test'));
-ol.appendChild(li);
+function dynamicList() {
+    var array =['week1/index.html', 'week2/index.html']
+    var ol = document.getElementById("ol");
+    for (i = 0; i < array.length; i++) {
+        var a = document.createElement("a"); 
+        var li = document.createElement("li");
+        a.setAttribute('href', array[i]);
+        a.text = array[i].split('/', 1) + " notes";
+        li.appendChild(a);
+        ol.appendChild(li);
+    }
+}
